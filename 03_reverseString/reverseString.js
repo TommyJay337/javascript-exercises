5c2564f90df3c2a39b63916897b0b5912a46da6a
TommyJay337 <thomas.c.johnston1998@gmail.com>
2025-04-16 10:55:40 -0600

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function(input) {
+
+    //console.log("Original input:", input);
+
+    let array = input.split("");
+
+    //console.log("Input after split:", array);
+
+    array.reverse();
+
+    //console.log("Input after reverse:", array);
+
+    let result = array.join("");
+
+    //console.log("Input after join:", result);
+
+    return result;
 
 };
+
+//console.log("Final result:", reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;

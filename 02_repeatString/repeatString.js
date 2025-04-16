9044323caefb2bf57986e831e600c5f6ae88b899
TommyJay337 <thomas.c.johnston1998@gmail.com>
2025-04-16 10:29:39 -0600

    Completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
 
+const repeatString = function(word, num) {
+
+    if (num < 0) {
+
+        return "ERROR"
+    }
+
+    else {
+        
+        let result = "";
+
+        for (let i = 0; i < num; i++ ){
+            result += word;
+        }
+
+         return result;
+    }     
+        
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
